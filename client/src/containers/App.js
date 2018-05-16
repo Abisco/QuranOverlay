b7a5756d5aa8efb8d78759e1664bf8c6a558aa9c
Abbass-Ali Siwjee <abu.siwjee@utoronto.ca>
2018-05-16 18:51:44 -0400

    Adjusted so cannot go 'off' the the limits of the verses, can only search for certain verses, added surah dictionary + names

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -3,6 +3,8 @@ import { connect } from "react-redux";
 
 import { grabVerse } from "../actions/quranVersesActions"
 
+import { surahs_info } from "../helpers/quran_info";
+
 import '../styles/App.css';
 
 class App extends Component {
@@ -31,6 +33,10 @@ class App extends Component {
           type = "SURAH"
           current_verse = ""
           current_surah = ""
+        } else {
+          type = "SURAH"
+          current_verse = ""
+          current_surah = ""
         }
       } else if (event.keyCode === 58 || event.keyCode === 186 || event.keyCode === 16) {
         //Colon, switch from surah to verse
@@ -42,29 +48,34 @@ class App extends Component {
           current_surah = self.props.quran_verses.verses[0].surah_id
         }
 
-        current_verse = (parseInt(current_verse) + 1).toString();
-
-        if (current_verse > self.props.quran_verses.quran_dictionary[current_surah]) {
-          current_surah = (parseInt(current_surah) + 1).toString();
-          current_verse = 1
+        if (parseInt(current_verse) !== self.props.quran_verses.quran_dictionary[114] || parseInt(current_surah) !== 114) {
+          if (parseInt(current_verse) + 1 > self.props.quran_verses.quran_dictionary[current_surah]) {
+            current_surah = (parseInt(current_surah) + 1).toString();
+            current_verse = 1
+          } else {
+            current_verse = (parseInt(current_verse) + 1).toString();
+          }
+  
+          self.props.dispatch(grabVerse(current_surah, current_verse));
         }
-
-        self.props.dispatch(grabVerse(current_surah, current_verse));
       } else if (event.keyCode === 37) {
         // Left arrow
         if (current_verse === "" || current_surah === "") {
           current_verse = self.props.quran_verses.verses[0].verse_id
           current_surah = self.props.quran_verses.verses[0].surah_id
-        } else {
-          current_verse = (parseInt(current_verse) - 1).toString();
-
-          if (current_verse < 1) {
-            current_surah = (parseInt(current_surah) - 1).toString();
-            current_verse = self.props.quran_verses.quran_dictionary[current_surah]
-          }
         }
 
-        self.props.dispatch(grabVerse(current_surah, current_verse));
+        if (parseInt(current_verse) !== 1 || parseInt(current_surah) !== 1) {
+          if (parseInt(current_verse) === 1 && parseInt(current_surah) !== 1) {
+            current_surah = (parseInt(current_surah) - 1).toString();
+            current_verse = self.props.quran_verses.quran_dictionary[current_surah]
+          } else if (parseInt(current_verse) !== 1) {
+            current_verse = (parseInt(current_verse) - 1).toString();
+          }
+
+          self.props.dispatch(grabVerse(current_surah, current_verse));
+        }
+        
       } else if (event.keyCode === 8) {
         // Backspace
         type = "SURAH"
@@ -75,8 +86,6 @@ class App extends Component {
       } else if (type === "SURAH") {
         current_surah += String.fromCharCode(event.keyCode)
       }
-      console.log(current_surah)
-      console.log(current_verse)
     })
   
     this.props.dispatch(grabVerse(1, 1));
@@ -84,9 +93,18 @@ class App extends Component {
   }
 
   render() {
+    var english_name = ""
+    var arabic_name = ""
+    if (surahs_info[this.props.quran_verses.verses[0].surah_id]) {
+      english_name = surahs_info[this.props.quran_verses.verses[0].surah_id].english
+      arabic_name = surahs_info[this.props.quran_verses.verses[0].surah_id].arabic
+    }
+  
     return (
       <div className="App">
         <div className="verse_container_third">
+          <h4>{arabic_name}</h4>
+          <h4>{english_name}</h4>
           <h1>{this.props.quran_verses.verses[0].arabic_ayah}</h1>
           <h3>{this.props.quran_verses.verses[0].shakir_ayah}</h3>
           <h5>({this.props.quran_verses.verses[0].surah_id}, {this.props.quran_verses.verses[0].verse_id})</h5>
