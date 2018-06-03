f4c2f5e62b7c0f2fbf689f0d214ef86c84a5d2c5
Abbass-Ali Siwjee <abu.siwjee@utoronto.ca>
2018-06-03 03:30:47 -0400

    Added dropdown for selecting a surah with default verse 0

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -5,17 +5,43 @@ import ColorPicker from 'rc-color-picker';
 import SettingsMenu from './SettingsMenu';
 import VerseSearch from './VerseSearch';
 
+import { grabVerse } from "../actions/quranVersesActions"
+
+import { surahs_info } from "../helpers/quran_info";
+
 class Navbar extends Component {
     constructor(props) {
         super(props);
 
+        this.selectSurah = this.selectSurah.bind(this);
+
         this.state = {
-        verse: {
-        }
+            verse_options: []
         };
     }
 
-    componentDidMount() {
+    componentWillMount() {
+        var surah_options = []
+
+        for (var i = 1; i < 115; i++) {
+            var option = {}
+            option.key = surahs_info[i].id
+            option.value = surahs_info[i].id
+            option.text = surahs_info[i].id + ". " + surahs_info[i].latin + " - " + surahs_info[i].english
+            surah_options.push(option)
+        }
+
+        this.props.dispatch({type: "SET_SURAH_OPTIONS", payload: surah_options})
+    }
+
+    selectSurah(e, value) {
+        var surah_number = value.value
+        var verse_number = 0
+        if (surah_number === "1" || surah_number === "9") {
+            verse_number = 1
+        }
+
+        this.props.dispatch(grabVerse(surah_number, verse_number));
     }
 
 
@@ -30,6 +56,9 @@ class Navbar extends Component {
                     <VerseSearch />
                 </div>
                 {current_search}
+                <div className="navbar_right_dropdown">
+                    <Dropdown placeholder='Select Surah' search selection autocomplete options={this.props.quran_verses.surah_options} onChange={this.selectSurah}/>
+                </div>
                 <div className="navbar_right_options">
                     <Dropdown item icon='question' simple direction='left'>
                         <Dropdown.Menu>
