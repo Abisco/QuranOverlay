bc31c2f1edb9bd711e6c97bf2c86998ab598180d
Abbass-Ali Siwjee <abu.siwjee@utoronto.ca>
2018-09-15 23:12:33 -0400

    Added all keyboard commands for Shortcuts

--- client/src/components/ShortcutsMenu.js
+++ client/src/components/ShortcutsMenu.js
@@ -34,12 +34,79 @@ class ShortcutsMenu extends Component {
         if (this.state.show_shortcuts) {
             keyboard_menu =<div className="keyboard_menu">
                                 <Header size='tiny' dividing>Keyboard Shortcuts</Header>
+                                <Header size='tiny'>Toggles</Header>
+                                <div className="keyboard_item">
+                                    <div className="keyboard_left">
+                                        <kbd>Shift</kbd> + <kbd>/</kbd>
+                                    </div>
+                                    <div className="keyboard_right">
+                                        Toggle Toolbar
+                                    </div>
+                                </div>
                                 <div className="keyboard_item">
                                     <div className="keyboard_left">
                                         <kbd>Shift</kbd> + <kbd>Q</kbd>
                                     </div>
                                     <div className="keyboard_right">
                                         Toggle Arabic
+                                    </div>
+                                </div>
+                                <div className="keyboard_item">
+                                    <div className="keyboard_left">
+                                        <kbd>Shift</kbd> + <kbd>E</kbd>
+                                    </div>
+                                    <div className="keyboard_right">
+                                        Toggle English
+                                    </div>
+                                </div>
+                                <Header size='tiny'>Verse Search</Header>
+                                <div className="keyboard_item">
+                                    <div className="keyboard_left">
+                                        <kbd>114</kbd> + <kbd>:</kbd> + <kbd>1</kbd>
+                                    </div>
+                                    <div className="keyboard_right">
+                                        Pull up Verse
+                                    </div>
+                                </div>
+                                <div className="keyboard_item">
+                                    <div className="keyboard_left">
+                                        <kbd>Backspace</kbd>
+                                    </div>
+                                    <div className="keyboard_right">
+                                        Clear Verse Search
+                                    </div>
+                                </div>
+                                <Header size='tiny'>Font Sizes</Header>
+                                <div className="keyboard_item">
+                                    <div className="keyboard_left">
+                                        <kbd>Alt</kbd> + <kbd>Q</kbd>
+                                    </div>
+                                    <div className="keyboard_right">
+                                        Increase Arabic Size
+                                    </div>
+                                </div>
+                                <div className="keyboard_item">
+                                    <div className="keyboard_left">
+                                        <kbd>Alt</kbd> + <kbd>W</kbd>
+                                    </div>
+                                    <div className="keyboard_right">
+                                        Decrease Arabic Size
+                                    </div>
+                                </div>
+                                <div className="keyboard_item">
+                                    <div className="keyboard_left">
+                                        <kbd>Alt</kbd> + <kbd>E</kbd>
+                                    </div>
+                                    <div className="keyboard_right">
+                                        Increase English Size
+                                    </div>
+                                </div>
+                                <div className="keyboard_item">
+                                    <div className="keyboard_left">
+                                        <kbd>Alt</kbd> + <kbd>R</kbd>
+                                    </div>
+                                    <div className="keyboard_right">
+                                        Decrease English Size
                                     </div>
                                 </div>
                             </div>
